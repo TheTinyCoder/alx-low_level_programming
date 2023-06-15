b0be474530fafde39c07391f40e4890fc57989c3
Xanadu <charawe.yw@gmail.com>
2023-06-15 14:45:25 +0300

    write a C function that prints to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function entry-point
+ *
+ * Description: 'function that prints to standard error'
+ * No parameters hence void
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(
+			stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	);
+	return (1);
+}
