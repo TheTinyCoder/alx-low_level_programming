09a3f4a967348b3db378cb87726308cbc89c4675
Xanadu <charawe.yw@gmail.com>
2023-06-15 13:51:26 +0300

    write a C function that prints using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function entry-point
+ *
+ * Description: 'function that prints a string with puts'
+ * No parameters hence void
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
