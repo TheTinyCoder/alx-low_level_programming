4327957d135999e437c1e7a079e4bacdc10e466d
Xanadu <charawe.yw@gmail.com>
2023-06-15 13:53:22 +0300

    write a C function that prints using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function entry-point
+ *
+ * Description: 'function that prints a string with printf'
+ * No parameters hence void
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
