0bca0ea5e3fc136d4a670f722349730448f341a1
Xanadu <charawe.yw@gmail.com>
2023-06-16 23:49:25 +0300

    edit function to conform to betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,36 +10,36 @@
 
 int main(void)
 {
-	int m, n, p, q, r, s;
+int m, n, p, q, r, s;
 
-	for (m = 48; m < 58; m++)
+for (m = 48; m < 58; m++)
+{
+	for (n = 48; n < 58; n++)
 	{
-		for (n = 48; n < 58; n++)
+		for (p = m; p < 58; p++)
 		{
-			for (p = m; p < 58; p++)
+			for (q = 48; q < 58; q++)
 			{
-				for (q = 48; q < 58; q++)
+				r = (m * 10) + n;
+				s = (p * 10) + q;
+				if (r < s)
 				{
-					r = (m * 10) + n;
-					s = (p * 10) + q;
-					if (r < s)
-					{
-						putchar(m);
-						putchar(n);
-						putchar(' ');
-						putchar(p);
-						putchar(q);
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(m);
+					putchar(n);
+					putchar(' ');
+					putchar(p);
+					putchar(q);
 					if (r == 98 && s == 99)
 					{
 						putchar('\n');
 						break;
 					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
+}
 	return (0);
 }
