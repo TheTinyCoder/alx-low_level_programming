89185a02a4a6012b5f5e8879c73163540772da06
Xanadu <charawe.yw@gmail.com>
2023-06-16 21:26:39 +0300

    write a C function that checks last digit of a random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - function entry-point
+ *
+ * Description: 'assigns random number to n and checks last digit value'
+ * No parameters hence void
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n%10 > 5)
+	{
+		printf("Last digit of %d is %d ", n, n%10);
+		printf("and is greater than 5\n");
+	}
+	else if (n%10 < 6 && n%10 != 0)
+	{
+		printf("Last digit of %d is %d ", n, n%10);
+		printf("and is less than 6 and not 0\n");
+	}
+	else 
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n%10);
+	}
+	return (0);
+}
