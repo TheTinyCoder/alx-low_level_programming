9d42796c101f5fefff244d0e846465a27386dab8
Xanadu <charawe.yw@gmail.com>
2023-06-16 17:21:19 +0300

    write a C function that checks if number is +ve or -ve

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - function entry-point
+ *
+ * Description: 'assigns a random number and determines if +ve, -ve or zero'
+ * No parameters hence void
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
+	}
+	return (0);
+}
