2590ca2be3ec4230e547fe904c7ecf84c34298d4
Xanadu <charawe.yw@gmail.com>
2023-06-20 21:43:22 +0300

    write function that prints the alphabet in lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet -  function entry-point
+ *
+ * Description: 'prints alphabet in lowercase x10 followed by \n'
+ * No parameters hence void
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 97; x < 123; x++)
+		{
+			_putchar(x);
+			if (x == 122)
+				_putchar('\n');
+		}
+	}
+}
