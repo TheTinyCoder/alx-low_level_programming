ae9d307b4014776de50df88401aaa2a68e9be09f
Xanadu <charawe.yw@gmail.com>
2023-06-22 21:12:16 +0300

    write function that prints sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main -  function entry-point
+ *
+ * Description: 'prints sum of multiples of 3/5 below 1024 followed by \n'
+ * No parameters hence void
+ * Return: void
+ */
+
+int main(void)
+{
+	int v, w, x, y, z = 0;
+
+	for (x = 1023; x > 0; x--)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+			z += x;
+	}
+
+	v = z, w = 1;
+	while (v > 9)
+	{
+		w *= 10;
+		v /= 10;
+	}
+
+	while (w > 1)
+	{
+		y = z / w;
+		_putchar(y + 48);
+		 z -= y * w;
+	}
+	_putchar(z + 48);
+	_putchar('\n');
+}
