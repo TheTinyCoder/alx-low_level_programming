3ae55df334c30125153b43f34e842ac9183e9ee2
Xanadu <charawe.yw@gmail.com>
2023-06-22 22:46:10 +0300

    write function that prints the first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  function entry-point
+ *
+ * Description: 'prints the first 50 Fibonacci numbers followed by \n'
+ * No parameters hence void
+ * Return: 0
+ */
+
+int main(void)
+{
+	int v = 1, w = 2, x;
+
+	for (x = 1; x < 26; x++)
+	{
+		fprintf(stdout, "%d", v);
+		_putchar(',');
+		_putchar(' ');
+		fprintf(stdout, "%d", w);
+		_putchar(',');
+		_putchar(' ');
+		v += w;
+		w += v;
+	}
+
+	return (0);
+}
