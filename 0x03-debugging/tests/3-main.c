33a98dc53ce7c700175c318d0ace1b7779234a1e
Xanadu <charawe.yw@gmail.com>
2023-06-24 00:36:45 +0300

    refactor condition and test

--- 0x03-debugging/tests/3-main.c
+++ 0x03-debugging/tests/3-main.c
@@ -23,5 +23,15 @@ int main(void)
 
     print_remaining_days(month, day, year);
 
+    month = 2;
+    day = 29;
+    year = 2000;
+    
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    
+    day = convert_day(month, day);
+    
+    print_remaining_days(month, day, year);
+
     return (0);
 }
