4d9171628fc9d4c19078ed99d5338af3d34c5007
Xanadu <charawe.yw@gmail.com>
2023-06-23 23:20:19 +0300

    write function that runs fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function entrypoint
+ *
+ * Description: fizz-buzz
+ * No parameters hence void
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			fprintf(stdout, "Fizz Buzz");
+		else if (i % 3 == 0)
+			fprintf(stdout, "Fizz");
+		else if (i % 5 == 0)
+			fprintf(stdout, "Buzz");
+		else
+			fprintf(stdout, "%d", i);
+
+		if (i != 100)
+			fprintf(stdout, " ");
+	}
+	fprintf(stdout, "\n");
+
+	return (0);
+}
