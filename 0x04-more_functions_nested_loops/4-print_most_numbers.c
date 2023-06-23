e44b19220beee1af745680e73765a02980ba14c9
Xanadu <charawe.yw@gmail.com>
2023-06-23 22:08:24 +0300

    write function that  prints from 0 to 9 except 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function entrypoint
+ *
+ * Description: prints from 0 to 9, except 2 & 4
+ * No parameter hence void
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
