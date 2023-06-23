82b1a9d6c105e20d7b4113d96b8b2402a1f25f64
Xanadu <charawe.yw@gmail.com>
2023-06-23 22:02:09 +0300

    write function that  prints from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - function entrypoint
+ *
+ * Description: prints from 0 to 9, followed by a new line
+ * No parameter hence void
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
