f02fdb85f26c65680c473d924c8d57c401aa9f07
Xanadu <charawe.yw@gmail.com>
2023-06-23 23:43:56 +0300

    edit to remove space for FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			fprintf(stdout, "Fizz Buzz");
+			fprintf(stdout, "FizzBuzz");
 		else if (i % 3 == 0)
 			fprintf(stdout, "Fizz");
 		else if (i % 5 == 0)
