a1e5da0b0e02ef6e8386c477a24b48bc90c721d0
Xanadu <charawe.yw@gmail.com>
2023-06-26 22:31:50 +0300

    write function that takes a pointer and changes value it points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function entry-point
+ *
+ * Description: takes a pointer & updates value it points to
+ * @n: pointer to integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
