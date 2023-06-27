f19ea7310e34e041cf3370b05706b910708acb60
Xanadu <charawe.yw@gmail.com>
2023-06-27 14:15:11 +0300

    write function that convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _atoi - function entry-point
+ *
+ * Description: converts string to integer
+ * @s: string; char array
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int number = 0, place_value = 1, plus = 0, minus = 0, x = 0, y;
+
+	while (s[x] != '\0')
+	{
+		if (s[x] == '+')
+			plus++;
+		if (s[x] == '-')
+			minus++;
+		if (s[x] > 47 && s[x] < 58)
+		{
+			if (s[x + 1] > 58 || s[x + 1] < 47)
+				break;
+			place_value *= 10;
+		}
+		x++;
+	}
+
+	for (y = 0; y <= x; y++)
+	{
+		if (s[y] > 47 && s[y] < 58)
+		{
+			number += ((s[y] - 48) * place_value);
+			place_value /= 10;
+		}
+	}
+
+	if (minus > plus)
+		number *= -1;
+
+	return (number);
+}
