ff102f57962af18c0f514c67f0cc1593be1fdf83
TheTinyCoder <charawe.yw@gmail.com>
2023-07-18 20:30:05 +0300

    refactor to loop to 52

--- 0x06-pointers_arrays_strings/tests/100-main.c
+++ 0x06-pointers_arrays_strings/tests/100-main.c
@@ -9,6 +9,8 @@
 int main(void)
 {
     char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+    char t[] = "Talk is cheap. Show me the code ([#CisFun, #CisAwesome])\n";
+
     char *p;
 
     p = rot13(s);
@@ -25,5 +27,9 @@ int main(void)
     printf("%s", p);
     printf("------------------------------------\n");
     printf("%s", s);
+    printf("------------------------------------\n");
+    p = rot13(t);
+    printf("%s", p);
+    printf("%s", t);
     return (0);
 }
