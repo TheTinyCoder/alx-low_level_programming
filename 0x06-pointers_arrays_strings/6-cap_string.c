e6149df575b94fc994be9c04c1b036e1b13f66d7
TheTinyCoder <charawe.yw@gmail.com>
2023-07-18 13:47:33 +0300

    write script that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *cap_string -  function entry-point
+ *
+ * Description: 'capitalizes all words of a string'
+ * @s: string
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	int k[14] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == k[j])
+			{
+				if (s[i + 1] > 96 && s[i + 1] < 123)
+					s[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (s);
+}
