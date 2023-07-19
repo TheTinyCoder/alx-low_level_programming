a4304737d3bc4c209afba5b13b79a441e12782ce
TheTinyCoder <charawe.yw@gmail.com>
2023-07-19 14:32:10 +0300

    write script that adds two numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * infinite_add -  function entry-point
+ *
+ * Description: 'adds two numbers'
+ * @n1: number as string
+ * @n2: number as string
+ * @r: buffer to store result
+ * @size_r: size of buffer
+ * Return: addition result as string
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0, k = 0, l, m, n;
+
+	for (; n1[i] != '\0'; i++)
+		;
+	for (; n2[j] != '\0'; j++)
+		;
+	if (i > size_r - 2 || j > size_r - 2)
+		return (0);
+	for (l = size_r - 2; l >= 0; l--)
+		r[l] = 0;
+	l = i > j ? i : j;
+	i--, j--;
+	printf("i: %d, j: %d, m: %d \n", i, j, l);
+	for (; l >= 0; l--)
+	{
+		m = i >= 0 ? n1[i] : 48;
+		n = j >= 0 ? n2[j] : 48;
+		r[l] = (((m + n) - 96 + k) % 10) + 48;
+		k = ((m + n) - 96 + k) / 10;
+		i--, j--;
+	}
+	return (r);
+}
