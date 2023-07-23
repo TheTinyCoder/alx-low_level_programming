1143f35f8ddb324febc2214d614af67f5a2b51aa
TheTinyCoder <charawe.yw@gmail.com>
2023-07-23 22:19:17 +0300

    write script that returns the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial -  function entry-point
+ *
+ * Description: 'returns the factorial of a given number'
+ * @n: integer
+ * Return: void
+ */
+
+int factorial(int n)
+{
+	int p;
+
+	if (n < 0)
+		return (-1);
+	else
+	{
+		if (n == 0)
+			return (1);
+		p = n;
+		p--;
+		return (n * factorial(p));
+	}
+}
