b75f354c3d86d0ea8d1647a9acbf009ff90751be
TheTinyCoder <charawe.yw@gmail.com>
2023-07-24 14:56:01 +0300

    write script that returns 1 if a string is a palindrome and 0 if not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * count_char -  function entry-point
+ *
+ * Description: 'prints a string in reverse'
+ * @s: pointer to a string
+ * Return: char
+ */
+
+int count_char(char *s, int count)
+{
+	if (*s == '\0')
+		return (count - 1);
+	count++;
+	s++;
+	return (count_char(s, count));
+}
+
+/**
+ * compare -  function entry-point
+ *
+ * Description: 'compares characters in s'
+ * @s: string
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+
+int compare(char *s, int count, int index)
+{
+	if (index >= count)
+		return (1);
+
+	if (s[index] == s[count])
+	{
+		index++;
+		count--;
+		return (compare(s, count, index));
+	}
+	return (0);	
+}
+
+/**
+ * is_palindrome -  function entry-point
+ *
+ * Description: 'checks if s is a palindrome'
+ * @s: integer
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int count, index;
+
+	count = count_char(s, 0);
+	index = 0;
+	if (s[index] == s[count])
+	{
+		index++;
+		count--;
+		return (compare(s, count, index));
+	}
+	return (0);
+}
