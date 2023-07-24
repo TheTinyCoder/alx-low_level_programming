6349517aeaab6806a614ef44c3198aeb663c2f0a
TheTinyCoder <charawe.yw@gmail.com>
2023-07-24 07:44:44 +0300

    write script that returns the power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -  function entry-point
+ *
+ * Description: 'returns the value of x to the power of y'
+ * @x: integer
+ * @y: integer
+ * Return: integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	y--;
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y));
+}
