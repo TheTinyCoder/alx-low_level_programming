876c1803e387ae2972b9fec01c32859df50cab04
TheTinyCoder <charawe.yw@gmail.com>
2023-07-25 10:28:25 +0300

    write and compile a program that adds arguments passed if integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function entry-point
+ * Description: adds arguments passed, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 on success, 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	long int a, b, c = 0;
+	char *temp;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	for (a = 1; a < argc; a++)
+	{
+		b = strtol(argv[a], &temp, 10);
+		if (*temp != '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		c += b;
+	}
+	printf("%ld\n", c);
+	return (0);
+}
