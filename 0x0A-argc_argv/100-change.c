0ee86136e44998c638d09543aa461e6953bf6c64
TheTinyCoder <charawe.yw@gmail.com>
2023-07-25 11:00:05 +0300

    write and compile a program that prints the minimum number of coins to make change for an amount of money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function entry-point
+ * Description: prints the minimum no. of coins to make change from cash
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 on success, 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int a [] = {25, 10, 5, 2, 1}, b, c, d = 0, e;
+
+	if (argc < 2 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	c = atoi(argv[1]);
+	if (c < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (b = 0; b < 5; b++)
+	{
+		if (c >= a[b])
+		{
+			d = (c / a[b]);
+			for (e = 0; e < 5; e++)
+			{
+				if (c % a[b] >= a[e])
+				{
+					d += ((c % a[b]) / a[e]);
+					d += ((c % a[b]) % a[e]);
+					break;
+				}
+			}
+			break;
+		}
+	}
+	printf("%d\n", d);
+	return (0);
+}
