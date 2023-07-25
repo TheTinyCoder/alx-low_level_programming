dcfef1f33e74a2f6d9b0fefec06f4fa50ed34154
TheTinyCoder <charawe.yw@gmail.com>
2023-07-25 10:02:35 +0300

    refactor and compile a program that multiplies two arguments passed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - function entry-point
@@ -15,6 +16,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", ((*argv[1] - 48) * (*argv[2] - 48)));
+	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	return (0);
 }
