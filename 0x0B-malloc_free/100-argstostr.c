eb100ea536dd4f9ebb22d67b5c185a550d4a64ab
TheTinyCoder <charawe.yw@gmail.com>
2023-07-26 12:28:24 +0300

    write and test script that concatenates all the arguments of program to string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *argstostr - function entry-point
+ *
+ * Description: frees a 2D grid previously allocated
+ * @ac: argument count
+ * @av: array of strings
+ * Return: pointer to string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 1;
+	char *a;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+	}
+
+	a = (char *)malloc(sizeof(char) * ac * len);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			a[k] = av[i][j];
+			k++;
+		}
+		a[k] = '\n';
+		k++;
+	}
+	return (a);
+}
