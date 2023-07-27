30bc38dc075ebfad41d99fe0153e41758d234fed
TheTinyCoder <charawe.yw@gmail.com>
2023-07-27 07:21:03 +0300

    write script that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_chcked - function entry-point
+ *
+ * Description: allocates memory using malloc
+ * @b: allocation size
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+
+	c = malloc(b);
+	if (c == NULL)
+		exit(98);
+	return (c);
+}
