bc5ac18183d656505075ce8a047d131a111e33d5
TheTinyCoder <charawe.yw@gmail.com>
2023-08-10 13:39:56 +0300

    creates a new struct instance through function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function entry-point
+ *
+ * Description: creates a new variable of type struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner
+ * Return: pointer to a struct of type dog_t:dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+
+	newDog = malloc(sizeof(name) + sizeof(age) + sizeof(owner));
+	if (newDog == NULL)
+		return (NULL);
+	newDog->name = name;
+	newDog->age = age;
+	newDog->owner = owner;
+	return (newDog);
+}
