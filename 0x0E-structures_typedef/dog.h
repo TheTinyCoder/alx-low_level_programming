20c477d6691ec1b1b9bcfc38e92e0cf29a492ffe
TheTinyCoder <charawe.yw@gmail.com>
2023-08-10 10:32:03 +0300

    creates struct dog and tests

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure entry-point
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner
+ *
+ * Description: struct that holds dog info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
