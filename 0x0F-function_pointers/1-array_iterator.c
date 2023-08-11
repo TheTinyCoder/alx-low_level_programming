365ffdf0984ee2c6d6a9a9b2e13599c53441b553
TheTinyCoder <charawe.yw@gmail.com>
2023-08-11 11:45:31 +0300

    creates program that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function entry-point
+ *
+ * Description: runs a function on every element of array
+ * @array: integer array
+ * @size: size of array
+ * @action: function to run
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
