81d8ee1d4f13d95acd42afec55f50372b53e9562
TheTinyCoder <charawe.yw@gmail.com>
2023-08-11 10:22:10 +0300

    creates program that calls function through pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function entry-point
+ *
+ * Description: prints name using function sppecified
+ * @name: name to print
+ * @f: pointer to function to use
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
