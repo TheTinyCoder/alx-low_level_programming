336ec66087ee81930b9cb6474208ae0b4b9c0b27
TheTinyCoder <charawe.yw@gmail.com>
2023-08-12 09:13:57 +0300

    write up functions for simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function entry-point
+ *
+ * Description: calculates sum of two integers
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - function entry-point
+ *
+ * Description: calculates difference of two integers
+ * @a: integer
+ * @b: integer
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - function entry-point
+ *
+ * Description: calculates product of two integers
+ * @a: integer
+ * @b: integer
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - function entry-point
+ *
+ * Description: calculates division of an integer by the other
+ * @a: integer
+ * @b: integer
+ * Return: division result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - function entry-point
+ *
+ * Description: calculates remainder of division of integer by another
+ * @a: integer
+ * @b: integer
+ * Return: remainder of division
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
