069db33a32bc72eaca6e0fa200c68037b9c094ad
TheTinyCoder <charawe.yw@gmail.com>
2023-08-14 08:42:04 +0300

    write program that prints all variadic parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function entry-point
+ *
+ * Description: prints all of its parameters
+ * @separator: string to separate parameters on print
+ * @n: number of variadic arguments
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list p;
+
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d",va_arg(p, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(p);
+	printf("\n");
+}
