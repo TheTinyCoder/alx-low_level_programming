71177cebdbc4d91579e6cdf170c0734cdbbd0d25
TheTinyCoder <charawe.yw@gmail.com>
2023-08-17 23:22:37 +0300

    generate 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,51 +1,20 @@
-; Disassembly of file: 101-hello_holberton.o
-; Thu Aug 17 22:51:17 2023
-; Type: ELF64
-; Syntax: NASM
-; Instruction set: 8086, x64
+; ----------------------------------------------------------------------------------------
+; Writes "Hello, Holberton" to the console using only system calls. Runs on 64-bit Linux only.
+; To assemble and run:
+;
+;   nasm -f elf64 101-hello_holberton.asm && gcc -no-pie -std=gnu89 101-hello_holberton.o -o hello
+; ----------------------------------------------------------------------------------------
 
-default rel
+          global    main
 
-global main: function
+          section   .text
+main:     mov       rax, 1                  ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 60                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-extern puts                                             ; near
-
-
-SECTION .text   align=1 exec                            ; section number 1, code
-
-main:   ; Function begin
-        push    rbp                                     ; 0000 _ 55
-        mov     rbp, rsp                                ; 0001 _ 48: 89. E5
-        lea     rax, [rel ?_001]                        ; 0004 _ 48: 8D. 05, 00000000(rel)
-        mov     rdi, rax                                ; 000B _ 48: 89. C7
-        call    puts                                    ; 000E _ E8, 00000000(PLT r)
-        mov     eax, 0                                  ; 0013 _ B8, 00000000
-        pop     rbp                                     ; 0018 _ 5D
-        ret                                             ; 0019 _ C3
-; main End of function
-
-
-SECTION .data   align=1 noexec                          ; section number 2, data
-
-
-SECTION .bss    align=1 noexec                          ; section number 3, bss
-
-
-SECTION .rodata align=1 noexec                          ; section number 4, const
-
-?_001:                                                  ; byte
-        db 48H, 65H, 6CH, 6CH, 6FH, 2CH, 20H, 48H       ; 0000 _ Hello, H
-        db 6FH, 6CH, 62H, 65H, 72H, 74H, 6FH, 6EH       ; 0008 _ olberton
-        db 00H                                          ; 0010 _ .
-
-
-SECTION .note.gnu.property align=8 noexec               ; section number 5, const
-
-        db 04H, 00H, 00H, 00H, 20H, 00H, 00H, 00H       ; 0000 _ .... ...
-        db 05H, 00H, 00H, 00H, 47H, 4EH, 55H, 00H       ; 0008 _ ....GNU.
-        db 02H, 00H, 01H, 0C0H, 04H, 00H, 00H, 00H      ; 0010 _ ........
-        db 00H, 00H, 00H, 00H, 00H, 00H, 00H, 00H       ; 0018 _ ........
-        db 01H, 00H, 01H, 0C0H, 04H, 00H, 00H, 00H      ; 0020 _ ........
-        db 01H, 00H, 00H, 00H, 00H, 00H, 00H, 00H       ; 0028 _ ........
-
-
+          section   .data
+message:  db        "Hello, Holberton", 10      ; note the newline at the end
