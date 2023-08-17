5a2cca2bf977392307c631d0ad3b97675c973c1a
TheTinyCoder <charawe.yw@gmail.com>
2023-08-17 22:23:44 +0300

    write script that runs before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * run_before_main - function entry-point
+ *
+ * Description: runs before the main function
+ * No parameters hence void
+ * Return: void
+ */
+
+void run_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
