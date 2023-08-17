c769db1e590eaf64dfe4311ef2e3a85c340f65b0
TheTinyCoder <charawe.yw@gmail.com>
2023-08-17 20:18:18 +0300

    write script that frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include "stdio.h"
+#include "string.h"
+
+/**
+ * free_list - function entry-point
+ *
+ * Description: frees a list_t list
+ * @head: pointer to singly linked list
+ * Return: address of node added to linked list
+ */
+
+void free_list(list_t *head)
+{
+	nodePtr temp1, temp2;
+
+	temp1 = head;
+
+	while (temp1 != NULL)
+	{
+		free(temp1->str);
+		temp2 = temp1;
+		temp1 = temp1->next;
+		free(temp2);
+	}
+}
