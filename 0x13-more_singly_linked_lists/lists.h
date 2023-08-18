8927ab74e43cde7aa8df725554b1ddf259e615eb
TheTinyCoder <charawe.yw@gmail.com>
2023-08-18 14:57:43 +0300

    define node struct and add prototype for print_listint

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+/**
+ * nodePtr - Typedef for struct list_s
+ */
+typedef struct list_s *nodePtr;
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	nodePtr next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+#endif
