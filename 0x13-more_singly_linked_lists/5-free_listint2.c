7476192525440a0bf69b899637234fdbd4df4f95
TheTinyCoder <charawe.yw@gmail.com>
2023-08-21 08:34:05 +0300

    write script that frees a linked list and sets head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include "stdio.h"
+#include "string.h"
+
+/**
+ * free_listint2 - function entry-point
+ *
+ * Description: frees a listint_t list and sets head to NULL
+ * @head: double pointer to singly linked list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	nodePtr temp1, temp2;
+
+	temp1 = (*head)->next;
+	while (temp1 != NULL)
+	{
+		temp2 = temp1;
+		temp1 = temp1->next;
+		free(temp2);
+	}
+	(*head) = NULL;
+}
