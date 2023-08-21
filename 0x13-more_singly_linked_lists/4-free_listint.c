0b0a64542812691579885a7786c86b176d33f88a
TheTinyCoder <charawe.yw@gmail.com>
2023-08-21 08:20:11 +0300

    write script that frees a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include "stdio.h"
+#include "string.h"
+
+/**
+ * free_listint - function entry-point
+ *
+ * Description: frees a listint_t list
+ * @head: pointer to singly linked list
+ * Return: address of node added to linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	nodePtr temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
