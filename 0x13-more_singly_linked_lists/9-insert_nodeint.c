4d31aeaf3772a1a6bd2b1f36fe04550691627ee7
TheTinyCoder <charawe.yw@gmail.com>
2023-08-28 08:34:45 +0300

    refactor and test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	while (temp && i <= idx)
 	{
-		if (i == idx - 1)
+		if (i == idx - 1 || idx == 0)
 		{
 			new->next = temp->next, temp->next = new;
 			break;
