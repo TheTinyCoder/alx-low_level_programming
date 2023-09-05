872321f8110d2b5031103d8a5796e37a9fda8dcc
TheTinyCoder <charawe.yw@gmail.com>
2023-09-05 09:48:32 +0300

    amend condition to compare string with wildcard(*) string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) && *(s2 + 1) != '*' && !*s1)
+		if (*(s2 + 1) && !*s1)
 			return (0);
 		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
