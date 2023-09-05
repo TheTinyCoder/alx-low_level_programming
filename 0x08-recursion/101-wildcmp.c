6e011d48c19a7809b3e7f8572d10c62dd9e0b0be
TheTinyCoder <charawe.yw@gmail.com>
2023-09-05 09:25:25 +0300

    compare string with wildcard(*) string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,8 +17,11 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1));
-	if (*s1 != *s2 && *(s2 - 1) == '*')
-		return (wildcmp(s1 + 1, s2));
+	{
+		if (*(s2 + 1) && *(s2 + 1) != '*' && !*s1)
+			return (0);
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
 	return (0);
 }
