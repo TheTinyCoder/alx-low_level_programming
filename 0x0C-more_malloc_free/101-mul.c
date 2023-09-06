6ac40d88e8bc011c8e436f37dca9bf87b55b6914
TheTinyCoder <charawe.yw@gmail.com>
2023-09-06 14:34:21 +0300

    refactor and test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 
 /**
- * is_digit - function entry-point
+ * digit_len - function entry-point
  *
- * Description: checks if all values in array are numbers
- * @arr: array
- * Return: length of array, 0 otherwise
+ * Description: finds length of number
+ * @arr: string array of numbers
+ * Return: length of number, 0 if one of the values is not a digit
  */
 
-int is_digit(char *arr)
+int digit_len(char *arr)
 {
 	int i = 0;
 
@@ -24,114 +24,113 @@ int is_digit(char *arr)
 
 
 /**
- * multiply_matrix - function entry-point
+ * allocate - function entry-point
  *
- * Description: multiplies 2D matrix
- * @a: integer array
- * @b: integer array
- * @a_c: length of a
- * @b_d: length of b
- * Return: result of multiplication
+ * Description: allocates memory of input size
+ * @size: size of int array to allocate
+ * Return: address pointing to memory allocated or NULL on failure
  */
 
-int **multiply_matrix(int *a, int *b, int a_c, int b_d)
+int *allocate(int size)
 {
-	int index_a, index_b = b_d - 1, temp = 0, tens, **w, x, y, z;
+	int *i;
 
-	w = malloc(sizeof(int *) * b_d);
-	if (w == NULL)
+	i =  malloc(sizeof(int) * size);
+	if (i == NULL)
 		return (NULL);
-	for (x = 0; x < b_d; x++)
+	return (i);
+}
+
+
+/**
+ * mul - function entry-point
+ *
+ * Description: multiplies numbers
+ * @num1: number (as int array)
+ * @num1_len: length of num1
+ * @num2: number (as int array)
+ * @num2_len:length of num2
+ * Return: pointer to matrix result of product
+ */
+
+int **mul(int *num1, int num1_len, int *num2, int num2_len)
+{
+	int **matrix, temp, width = num2_len + num1_len, x, y, z;
+	int index_1, index_2 = num2_len - 1, place_value;
+
+	matrix = malloc(sizeof(int *) * num2_len);
+	if (matrix == NULL)
+		return (NULL);
+	for (x = 0; x < num2_len; x++)
 	{
-		w[x] = malloc(sizeof(int) * (a_c + b_d));
-		if (w[x] == NULL)
+		matrix[x] = malloc(sizeof(int) * width);
+		if (matrix[x] == NULL)
 		{
 			for (; x >= 0; x--)
-				free(w[x]);
-			free(w);
+				free(matrix[x]);
+			free(matrix);
 			return (NULL);
 		}
-	}
-	for (x = 0; x < b_d; x++)
-	{
-		index_a = a_c - 1, tens = 1;
-		for (y = a_c + b_d - 1; y >= 0; y--)
-		{
-			if (index_a < 0)
-			{
-				if (temp > 0)
-					w[x][y] = temp;
-				break;
-			}
-			if (x > 0 && tens == 1)
-			{
-				for (z = x; z > 0; z--)
-					w[x][y] = 0, y--;
-				tens = 0;
-			}
-			w[x][y] = ((a[index_a] * b[index_b]) + temp) % 10;
-			temp = (a[index_a] * b[index_b]) / 10;
-			index_a--;
-		}
-		index_b--;
-	}
-	return (w);
-}
-
-
-/**
- * sum_matrix - function entry-point
- *
- * Description: sums up 2D matrix
- * @matrix: 2D integer array
- * @width: columns of matrix
- * @height: rows of matrix
- * Return: result of multiplication
- */
-
-int *sum_matrix(int **matrix, int width, int height)
-{
-	int *a, temp = 0, w, x, y, z;
-
-	a = malloc(sizeof(int) * width);
-	if (a == NULL)
-		return (NULL);
-	for (w = 0; w < width; w++)
-		a[w] = matrix[0][w];
-	for (x = 1; x < height; x++)
-	{
+		place_value = 1, temp = 0, index_1 = num1_len - 1;
 		for (y = width - 1; y >= 0; y--)
 		{
-			z = (a[y] + matrix[x][y] + temp);
-			a[y] = z % 10;
-			temp = z / 10;
+			if (index_1 < 0 && temp > 0)
+				matrix[x][y] = temp, temp = 0;
+			else if (index_1 < 0)
+				matrix[x][y] = 0;
+			if (x > 0 && place_value == 1)
+			{
+				for (z = x; z > 0; z--)
+					matrix[x][y] = 0, y--;
+				place_value = 0;
+			}
+			if (index_1 >= 0)
+			{
+				matrix[x][y] = ((num1[index_1] * num2[index_2]) + temp) % 10;
+				temp = (num1[index_1] * num2[index_2]) / 10;
+			}
+			index_1--;
 		}
+		index_2--;
 	}
-	return (a);
+	return (matrix);
 }
 
 
 /**
- * print_array - function entry-point
+ * _sum - function entry-point
  *
- * Description: prints an array
- * @arr: array
- * @size: length of array
+ * Description: sums matrix arrays and prints result
+ * @matrix: pointer to int arrays
+ * @width: columns of matrix
+ * @height: rows of matrix
  * Return: void
  */
 
-void print_array(int *arr, int size)
+void _sum(int **matrix, int width, int height)
 {
-	int e;
+	int *sum, temp = 0, x, y, z;
 
-	for (e = 0; e < size; e++)
+	sum = allocate(width + height);
+	for (x = 0; x < width + height; x++)
+		sum[x] = 0;
+	for (y = 0; y < height; y++)
 	{
-		if (arr[e] == 0 && e == 0 && arr[e + 1] != 0)
+		for (z = width + height - 1; z >= 0; z--)
+		{
+			x = sum[z] + matrix[y][z] + temp;
+			sum[z] = x % 10;
+			temp = x / 10;
+		}
+	}
+	for (z = 0; z < width + height; z++)
+	{
+		if (sum[z] == 0 && z == 0)
 			continue;
-		printf("%d", arr[e]);
+		printf("%d", sum[z]);
 	}
 	printf("\n");
-
+	free(sum);
 }
 
 
@@ -141,43 +140,32 @@ void print_array(int *arr, int size)
  * Description: multiplies two positive numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 on success
+ * Return: product result
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int a = 0, b = 0, *c, *d, e, f, g, **result, *sum_result;
+	int num1_len = 0, num2_len = 0, **mul_result, *num1, *num2, x, y;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	if (!is_digit(argv[1]))
-		printf("Error\n"), exit(98);
-	if (!is_digit(argv[2]))
-		printf("Error\n"), exit(98);
-	if (argv[1][0] == 48 || argv[2][0] == 48)
+	if (argv[1][0] == 0 || argv[2][0] == 0)
 		printf("0\n"), exit(0);
-	a = is_digit(argv[1]), b = is_digit(argv[2]), g = a + b;
-	c = malloc(sizeof(int) * a), d = malloc(sizeof(int) * b);
-	if (c == NULL || d == NULL)
-	{
-		free(c), free(d);
-		printf("Error\n");
-		exit(98);
-	}
-	for (e = 0; e < a; e++)
-	{
-		c[e] = (argv[1][e] - 48);
-	}
-	for (f = 0; f < b; f++)
-	{
-		d[f] = (argv[2][f] - 48);
-	}
-	result = a > b ? multiply_matrix(c, d, a, b) : multiply_matrix(d, c, b, a);
-	sum_result = a > b ? sum_matrix(result, g, b) : sum_matrix(result, g, a);
-	print_array(sum_result, g);
-	g = a > b ? b : a;
-	for (f = 0; f < g; f++)
-		free(result[f]);
-	free(c), free(d), free(result), free(sum_result);
+	num1_len = digit_len(argv[1]), num2_len = digit_len(argv[2]);
+	if (num1_len == 0 || num2_len == 0)
+		printf("Error\n"), exit(98);
+	num1 = allocate(num1_len), num2 = allocate(num2_len);
+	for (x = 0; x < num1_len; x++)
+		num1[x] = argv[1][x] - 48;
+	for (x = 0; x < num2_len; x++)
+		num2[x] = argv[2][x] - 48;
+	mul_result = num1_len > num2_len ? mul(num1, num1_len, num2, num2_len)
+		: mul(num2, num2_len, num1, num1_len);
+	num1_len > num2_len ? _sum(mul_result, num1_len, num2_len)
+		: _sum(mul_result, num2_len, num1_len);
+	y = num1_len < num2_len ? num1_len : num2_len;
+	for (x = 0; x < y; x++)
+		free(mul_result[x]);
+	free(mul_result), free(num1), free(num2);
 	return (0);
 }
