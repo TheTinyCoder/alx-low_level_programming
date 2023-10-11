cc80215223abeebcee5ea7c9c9a29099b9d94f21
TheTinyCoder <charawe.yw@gmail.com>
2023-10-11 09:32:57 +0300

    use LD_PRELOAD to load custom rand

--- 0x18-dynamic_libraries/101-make_me_win.c
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -2,10 +2,10 @@
 
 int rand()
 {
-	static int count = -1;
+	static int count;
 	int numbers[6] = {8, 8, 7, 9, 23, 74};
 
 	if (count < 6)
 		return (numbers[count++]);
-	return (numbers[4]);
+	return (numbers[5]);
 }
