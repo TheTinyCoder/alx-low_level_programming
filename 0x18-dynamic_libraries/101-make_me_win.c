46b84ad15878c52aed21c4e07ec3faa9dd4d6585
TheTinyCoder <charawe.yw@gmail.com>
2023-10-11 09:35:51 +0300

    use LD_PRELOAD to load custom rand

--- 0x18-dynamic_libraries/101-make_me_win.c
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 
-int rand()
+/**
+ * rand - Overrides the standard library's rand function to ensure
+ * winning numbers in the Giga Millions game.
+ *
+ * When called, it returns a sequence of predetermined winning numbers.
+ *
+ * Return: The next winning number in the sequence.
+ */
+
+int rand(void)
 {
 	static int count;
 	int numbers[6] = {8, 8, 7, 9, 23, 74};
